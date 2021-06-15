aaf64c2879dba0fd2f73014f43a12b108a9520bf
Lobiplayer <jrobinmohan@gmail.com>
2021-06-15 13:03:51 +0800

    fixed netlify deploy error

--- src/components/UploadImageComponent.js
+++ src/components/UploadImageComponent.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios'
 import { toast } from 'react-toastify';
-import { Button, Form, FormGroup, Input } from 'reactstrap';
+import { Button, Form, FormGroup } from 'reactstrap';
 import Dropzone from 'react-dropzone'
 
 const UploadImageComponent = () => {
